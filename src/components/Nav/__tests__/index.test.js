9643fabf849e67ac85e602740667e02d3cc90daf
Sparkle Party <lexi.slovik@gmail.com>
2022-03-17 18:04:28 -0700

    add ContactForm component

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -5,16 +5,30 @@ import Nav from '..';
 
 afterEach(cleanup);
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+  ]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 // describe function is not necessary for the test to run
 // it is used to organize tests into sections that are labeled with the element being tested
 describe('Nav component', () => {
     // baseline
     it('renders', () => {
-        render(<Nav />);
+        render(<Nav 
+            categories={categories} 
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+            />);
     })
     // snapshot
     it('matches snapshot', () => {
-        const { asFragment } = render(<Nav />);
+        const { asFragment } = render(<Nav 
+            categories={categories} 
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+            />);
         expect(asFragment()).toMatchSnapshot();
     })
 })
@@ -22,17 +36,25 @@ describe('Nav component', () => {
 describe('camera emoji is visible', () => {
     it('inserts emoji into h2', () => {
         // Arrange
-        const { getByLabelText } = render(<Nav />);
+        const { getByLabelText } = render(<Nav 
+            categories={categories} 
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+            />);
         // Assert
         // test the emoji's accessibility features by querying the element by its aria-label camera
         expect(getByLabelText('camera')).toHaveTextContent('📸');
     })
 })
 
-describe('inks are visible', () => {
+describe('links are visible', () => {
     it('inserts text into the links', () => {
         // Arrange
-        const { getByTestId } = render(<Nav />);
+        const { getByTestId } = render(<Nav 
+            categories={categories} 
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+            />);
         // Assert
         expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
         expect(getByTestId('about')).toHaveTextContent('About Me');
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,15 +2,16 @@ import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav (props) {
-    useEffect(() => {
-        document.title = capitalizeFirstLetter(currentCategory.name);
-    }, [currentCategory]);
     
     const {
         categories =[],
         setCurrentCategory,
         currentCategory
     } = props;
+
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
 
     return (
         <header>
